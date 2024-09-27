dd8562e7841bc1f1fbd63db00ff9365eff03c1f5
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-27 10:20:53 -0400

    Added Content and changed the colors

--- interactive.js
+++ interactive.js
@@ -1,22 +1,59 @@
-color = ["violet", "green", "aqua","AliceBlue","Bisque","BlueViolet","Crimson","Gainsboro","LightCoral","MediumAquaMarine","black"
+color = ["violet", "green", "aqua","AliceBlue","Bisque","BlueViolet","Crimson","Gainsboro","LightCoral","MediumAquaMarine"
     ,"PaleTurquoise","Sienna","Silver","Turquoise","ForestGreen","FloralWhite","DarkTurquoise","Chocolate","BlanchedAlmond","LightPink","LightSalmon","red","Green","Blue",
     "Aqua","purple","Orange","Pink","Yellow","Blue"];
 
-background = ["red","Green","Blue"]
-randomback=Math.floor(Math.random() * background.length);
+
+    
+
+    
+
+
+
+
+//randomback=Math.floor(Math.random() * background.length);
 
 const randomint=Math.floor(Math.random() * color.length);
 document.getElementById("index").style.color = color[randomint];
-document.getElementById("index2").style.color = color[randomint];
-document.getElementById("index3").style.color = color[randomint];
-document.getElementById("index4").style.color = color[randomint];
-document.getElementById("index5").style.color = color[randomint];
+document.body.style.backgroundColor ="Black";
+
+//index2 
+document.getElementById("index2").style.background = color[randomint];
+document.getElementById("index2").style.color = "black";
+
+//index3
+document.getElementById("index3").style.background = color[randomint];
+document.getElementById("index3").style.color = "black";
+//index4
+document.getElementById("index4").style.background = color[randomint];
+document.getElementById("index4").style.color = "Black";
+// index5
+document.getElementById("index5").style.background = color[randomint];
+document.getElementById("index5").style.color = "Black";
+//index6
+
 document.getElementById("index6").style.color = color[randomint];
+//index7
+
 document.getElementById("index7").style.color = color[randomint];
+
+//index8
+
 document.getElementById("index8").style.color = color[randomint];
+
+//index9
+
 document.getElementById("index9").style.color = color[randomint];
+//index10
+
 document.getElementById("index10").style.color = color[randomint];
-document.body.style.backgroundColor =background[randomback];
+//index11
+document.getElementsByClassName("container").style.color = color[randomint];
+
+// background
+
+
+
+
 
 
 
