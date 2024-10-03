48b330061a2e1864fa28f67f404f8b8a355f6d81
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-02 22:58:27 -0400

    added interactivty

--- clubs.js
+++ clubs.js
@@ -1,8 +1,39 @@
+document.getElementById("index2").style.background = "aqua";
+document.getElementById("index3").style.background = "aqua";
+document.getElementById("index4").style.background = "aqua";
+document.getElementById("index5").style.background = "aqua";
+document.getElementById("Button").style.background = "aqua";
+
 function getOption() {
     selectElement = 
           document.querySelector('#color');
     output = selectElement.value;
-    document.querySelector('.output').textContent = output; 
-    // above borrowed from https://www.geeksforgeeks.org/how-to-get-selected-value-in-dropdown-list-using-javascript/
     
+    // above borrowed from https://www.geeksforgeeks.org/how-to-get-selected-value-in-dropdown-list-using-javascript/
+    if(output=="default"){
+        output = "black";
+    }
+    document.getElementById("Button").style.color = output;
+ 
+   
+
+    document.getElementById("color").style.color = output;
+    document.getElementById("index2").style.color = output;
+   
+
+
+    document.getElementById("index3").style.color = output;
+   
+
+
+    document.getElementById("index4").style.color = output;
+  
+
+    document.getElementById("index5").style.color = output;
+   
+
+    document.getElementById("index6").style.color = output;
+    document.getElementById("index7").style.color = output;
+    document.getElementById("index8").style.color = output;
+    document.getElementById("index9").style.color = output;
 }
