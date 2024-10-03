b6f5c2057f666c819767aff6a96e5226ffb70f38
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-02 23:42:44 -0400

    added textbox

--- clubs.js
+++ clubs.js
@@ -37,3 +37,41 @@ function getOption() {
     document.getElementById("index8").style.color = output;
     document.getElementById("index9").style.color = output;
 }
+function Searchbox(){
+    //
+    selectElement = 
+          document.querySelector('#Search');
+    output = selectElement.value;
+       // above borrowed from https://www.geeksforgeeks.org/how-to-get-selected-value-in-dropdown-list-using-javascript/
+
+    if(output=="default"){
+        output = "black";
+    }
+    document.getElementById("Button").style.color = output;
+ 
+   
+
+    document.getElementById("color").style.color = output;
+    document.getElementById("index2").style.color = output;
+   
+
+
+    document.getElementById("index3").style.color = output;
+   
+
+
+    document.getElementById("index4").style.color = output;
+  
+
+    document.getElementById("index5").style.color = output;
+   
+
+    document.getElementById("index6").style.color = output;
+    document.getElementById("index7").style.color = output;
+    document.getElementById("index8").style.color = output;
+    document.getElementById("index9").style.color = output;
+
+    document.getElementById('Search').value = "";
+
+ 
+}
